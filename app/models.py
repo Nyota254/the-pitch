79f0bae81211a086be3ef73c51538b2d35dfe457
Nyota254 <knyota66@gmail.com>
2019-09-14 10:15:39 +0300

    add the password hashing fuctions to the models users

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     '''
@@ -8,9 +9,28 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String,unique = True,nullable = False)
     email = db.Column(db.String,unique = True,nullable = False)
-    password_hash = db.Column(db.String,nullable=False)
     bio = db.Column(db.String(255))
     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    password_hash = db.Column(db.String,nullable=False)
+    @property
+    def password(self):
+        '''
+        Raises error when someone trys to read the password
+        '''
+        raise AttributeError('You cannot read the password attribute')
+
+    @property.setter
+    def password(self,password):
+        '''
+        Generates password hash
+        '''
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        '''
+        confirms password equal to the password hash during login
+        '''
+        check_password_hash(self.password_hash,password)
 
 class Pitch(db.Model):
     '''

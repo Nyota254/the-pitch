d2ef0f6f7145f2f099ecae094d0923f0c350c6b4
Nyota254 <knyota66@gmail.com>
2019-09-14 09:54:28 +0300

    add the formlogic in forms.py file using wtf form

---
+++ app/auth/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import Email,Required,EqualTo,ValidationError
+from wtforms import StringField,SubmitField,PasswordField
+from ..models import User
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Enter your Username',validators=[Required()])
+    email = StringField('Please Enter Your Email',validators=[Email(),Required()])
+    password = PasswordField('Please Enter Your Password',validators=[Required(),EqualTo('confirm_password',message='Passwords do not match')])
+    confirm_password = PasswordField('Please Retype Your Password',validators=[Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError('There is a user already registered with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken please pick another')
+        
+
+

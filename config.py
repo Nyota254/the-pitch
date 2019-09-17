1af34cbd44c41b80529a2118cfd1fe5c88c60007
Nyota254 <knyota66@gmail.com>
2019-09-17 14:26:18 +0300

    Deploying to heroku

--- config.py
+++ config.py
@@ -1,22 +1,31 @@
+import os
 class Config:
     '''
     Primary configurations for the application
     '''
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://kevin:1234@localhost/pitchesdb"
-    SECRET_KEY = 'testkey'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 class DevConfig(Config):
     '''
     Development Configuratons
     '''
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://kevin:1234@localhost/pitchesdb"
     DEBUG = True
 class ProdConfig(Config):
     '''
     Production Configurations
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+class TestConfig(Config):
+    '''
+    Test configurations
+    '''
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://kevin:1234@localhost/pitchesdb_test"
+
 
 configurations = {
     'development':DevConfig,
-    'production':ProdConfig
+    'production':ProdConfig,
+    'test':TestConfig
 }
